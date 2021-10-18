d7a12fc20025e41b825470553e8201c343ae5d40
Aaron Ngo <aaron.vngo@gmail.com>
2021-10-18 15:48:40 +1100

    setting up

---
+++ screens/StartingScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const StartingScreen = (props) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+export default StartingScreen;
