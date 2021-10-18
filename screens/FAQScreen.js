c6cb4cb40a35be465c2f2a34d557501202805de2
dhanitalistra <dhanitalistra@users.noreply.github.com>
2021-10-18 22:21:02 +0700

    Completed FAQ page, however menu bar will be needed

--- screens/FAQScreen.js
+++ screens/FAQScreen.js
@@ -6,19 +6,27 @@ const FAQScreen = () => {
     <View style={stylesFAQ.faqContainer}>
 
         {/* Heading */}
-        <Text>FAQ</Text>
+        <Text style={stylesFAQ.faqHeading}>FAQ</Text>
 
-        {/* Questions */}
-        <View style={stylesFAQ.questionContainer}>
-            <Text>How do I connect to a different Lotus?</Text>
-            <Text>Navigate to your account page and press “leave lotus”</Text>
-        </View>
+        <Text style={stylesFAQ.faqQuestions}>How do I connect to a different Lotus?</Text>
+        <Text style={stylesFAQ.faqAnswer}>Navigate to your account page and press “leave lotus”</Text>       
 
-        <View style={stylesFAQ.questionContainer}>
-            <Text>How do I connect to a different Lotus?</Text>
-            <Text>Navigate to your account page and press “leave lotus”</Text>
-        </View>
+        <Text style={stylesFAQ.faqQuestions}>What does the colour on the main page mean?</Text>
+        <Text style={stylesFAQ.faqAnswer}>This is the colour of the day chosen by another family member</Text>
 
+        <Text style={stylesFAQ.faqQuestions}>How do I choose the colour of the day?</Text>
+        <Text style={stylesFAQ.faqAnswer}>When it is your turn to choose, the dropper icon
+        will appear on the top left corner of all screens.
+        Press the dropper icon to be taken to the choose colour page.</Text>
+
+        <Text style={stylesFAQ.faqQuestions}>When do I choose the colour of the day?</Text>
+        <Text style={stylesFAQ.faqAnswer}>You will be notified when it is your turn and
+        the main page will also let you know</Text>
+
+        <Text style={stylesFAQ.faqQuestions}>What do the icons on the pick colour page mean?</Text>
+        <Text style={stylesFAQ.faqAnswer}>These icons are used to increase the accessibility of the app and cater to those 
+        who are colourblind</Text>
+        
     </View>
     );
 };
@@ -26,14 +34,28 @@ const FAQScreen = () => {
 const stylesFAQ = StyleSheet.create({
     faqContainer: {
         flex: 1,
-        backgroundColor: "#fefefe",
+        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+        padding: 40,
+        paddingTop: 5
     },
-    questionContainer:{
-        marginTop: 2,
-        textAlign: "center"
+    faqHeading:{
+        fontSize: 35, 
+        fontWeight: "bold",
+        marginBottom: 30,
+        textAlign:"justify"
     },
+    faqQuestions:{
+        fontWeight:"bold",
+        fontSize: 18,
+        textAlign:"center"
+    }, 
+    faqAnswer:{
+        textAlign: "center",
+        fontSize: 18,
+        marginBottom: 30
+    }
     
   });
 
