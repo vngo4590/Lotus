bb933fc25e269c4f1960a76aa05cf184ad0a72a5
dhanitalistra <dhanitalistra@users.noreply.github.com>
2021-10-21 20:21:25 +0700

    added tutorial content. need to change background color styling

--- screens/TutorialPage.js
+++ screens/TutorialPage.js
@@ -6,18 +6,33 @@ import {Image} from "react-native";
 const TutorialPage = ({navigation}) => {
   return (
     <Onboarding
+      titleStyles={{fontWeight: 'bold', color: '#fff'}}
+      textStyle={{ color: '#fff', marginTop: 0, fontWeight: 'bold' }}
+      imageContainerStyles={{paddingBottom:0}}
       pages={[
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('../assets/tutorial_page_1.png')} />,
-          title: 'Onboarding1',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          backgroundColor: '#F4456F',
+          image: <Image source={require('../assets/tutorialscreen/screen1.png')} />,
+          title: 'Device Connection',
+          subtitle: 'Enter code given by elder with physical device',
         },
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('../assets/tutorial_page_2.png')} />,
-          title: 'Onboarding2',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          backgroundColor: '#F4456F',
+          image: <Image source={require('../assets/tutorialscreen/screen2.png')} />,
+          title: 'Colour of the Day',
+          subtitle: 'This page will light up with the colour of the day',
+        },
+        {
+          backgroundColor: '#F4456F',
+          image: <Image source={require('../assets/tutorialscreen/screen3.png')} />,
+          title: 'Connected Accounts',
+          subtitle: 'View your family members on your account page',
+        },
+        {
+          backgroundColor: '#F4456F',
+          image: <Image source={require('../assets/tutorialscreen/screen4.png')} />,
+          title: 'Colour Picker',
+          subtitle: 'This page and icon will appear when it is your turn to choose',
         },      
       ]}
     />
