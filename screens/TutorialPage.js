93b0605c54927c0255f97c591abb1e7f8f31a65a
dhanitalistra <dhanitalistra@users.noreply.github.com>
2021-10-21 20:30:35 +0700

    tutorial section completed. cannot change default slider/swipe structure

--- screens/TutorialPage.js
+++ screens/TutorialPage.js
@@ -6,30 +6,36 @@ import {Image} from "react-native";
 const TutorialPage = ({navigation}) => {
   return (
     <Onboarding
-      titleStyles={{fontWeight: 'bold', color: '#fff'}}
-      textStyle={{ color: '#fff', marginTop: 0, fontWeight: 'bold' }}
+      // PINK STYLING TUTORIAL for ref
+      // titleStyles={{fontWeight: 'bold', color: '#fff'}}
+      // textStyle={{ color: '#fff', marginTop: 0, fontWeight: 'bold' }}
+      titleStyles={{fontWeight: 'bold'}}
+      textStyle={{marginTop: 0, fontWeight: 'bold' }}
       imageContainerStyles={{paddingBottom:0}}
+
+      containerStyles={{padding: 70}}
+
       pages={[
         {
-          backgroundColor: '#F4456F',
+          backgroundColor: '#fff',
           image: <Image source={require('../assets/tutorialscreen/screen1.png')} />,
           title: 'Device Connection',
           subtitle: 'Enter code given by elder with physical device',
         },
         {
-          backgroundColor: '#F4456F',
+          backgroundColor: '#fff',
           image: <Image source={require('../assets/tutorialscreen/screen2.png')} />,
           title: 'Colour of the Day',
           subtitle: 'This page will light up with the colour of the day',
         },
         {
-          backgroundColor: '#F4456F',
+          backgroundColor: '#fff',
           image: <Image source={require('../assets/tutorialscreen/screen3.png')} />,
           title: 'Connected Accounts',
           subtitle: 'View your family members on your account page',
         },
         {
-          backgroundColor: '#F4456F',
+          backgroundColor: '#fff',
           image: <Image source={require('../assets/tutorialscreen/screen4.png')} />,
           title: 'Colour Picker',
           subtitle: 'This page and icon will appear when it is your turn to choose',
