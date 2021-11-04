f21d8c3ecd7ddb9c66b4e52d079e976c0331cff7
Aaronn <aaron.vngo@gmail.com>
2021-11-04 15:31:51 +1100

    Changed Margin

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Switch, Dimensions } from "react-native";
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
 import DefaultContainer from "../components/DefaultContainer";
 import DefaultText from "../components/DefaultText";
 import DefaultTitle from "../components/DefaultTitle";
 import Card from "../components/Card";
 import MainButton from "../components/MainButton.android";
-
 
 // FCE6E1 light pink
 // F4456F dark pink
@@ -15,19 +14,17 @@ const SettingsScreen = (props) => {
   const [isEnabled, setIsEnabled] = useState(false);
   const [isEnabled2, setIsEnabled2] = useState(false);
 
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-  const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const toggleSwitch2 = () => setIsEnabled2((previousState) => !previousState);
 
   const [selectedValue, setSelectedValue] = useState("java");
 
   return (
     <DefaultContainer style={styles.container}>
-
       <DefaultTitle style={styles.mainHeader}>Settings</DefaultTitle>
 
       {/* ======== REMINDER SECTION ==========*/}
       <Card style={styles.cardReminder}>
-
         {/* -daily reminder-*/}
         <View style={styles.cardReminderContent}>
           <DefaultText style={styles.cardText}>Daily Reminder:</DefaultText>
@@ -56,7 +53,6 @@ const SettingsScreen = (props) => {
 
       {/* ======== ADDITIONAL SETTINGS SECTION ==========*/}
       <Card style={styles.cardAdditionalSettings}>
-
         {/* -LANGUAGE-*/}
         <View style={styles.cardReminderContent}>
           <DefaultText style={styles.cardText2}>Language:</DefaultText>
@@ -65,13 +61,14 @@ const SettingsScreen = (props) => {
             <Picker
               selectedValue={selectedValue}
               style={styles.dropdown}
-              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
             >
               <Picker.Item label="English" value="EN" />
               <Picker.Item label="Chinese" value="CN" />
               <Picker.Item label="Spanish" value="ES" />
               <Picker.Item label="Hindi" value="HI" />
-
             </Picker>
           </View>
         </View>
@@ -84,11 +81,12 @@ const SettingsScreen = (props) => {
             <Picker
               selectedValue={selectedValue}
               style={styles.dropdown}
-              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
             >
               <Picker.Item label="Elderly" value="Elderly" />
               <Picker.Item label="Everyone" value="Everyone" />
-
             </Picker>
           </View>
         </View>
@@ -101,7 +99,9 @@ const SettingsScreen = (props) => {
             <Picker
               selectedValue={selectedValue}
               style={styles.dropdown}
-              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
             >
               <Picker.Item label="Daily" value="Daily" />
               <Picker.Item label="Every 2 days" value="Every 2 days" />
@@ -115,15 +115,12 @@ const SettingsScreen = (props) => {
           <Text style={styles.additionalText}>FAQ and Support</Text>
           <Text style={styles.additionalText}>Privacy Policy</Text>
         </View>
-        
       </Card>
 
-        {/* -APPLY CHANGES BUTTON-*/}
+      {/* -APPLY CHANGES BUTTON-*/}
       <View style={styles.buttonCustom}>
         <MainButton>Apply Changes</MainButton>
       </View>
-
-
     </DefaultContainer>
   );
 };
@@ -136,71 +133,67 @@ const styles = StyleSheet.create({
   },
   mainHeader: {
     marginBottom: 30,
-    marginTop: 75
+    marginTop: 20,
   },
-  cardText:{
+  cardText: {
     fontSize: 20,
-    marginBottom: 10
+    marginBottom: 10,
   },
   cardReminder: {
     alignItems: "flex-start",
     height: 120,
     width: "100%",
     padding: 20,
-    marginBottom: 30
+    marginBottom: 30,
   },
-  cardReminderContent:{
-    flex: 2, 
+  cardReminderContent: {
+    flex: 2,
     flexDirection: "row",
-    flexWrap:'wrap',
+    flexWrap: "wrap",
     justifyContent: "space-between",
     height: 10,
     width: "100%",
-    padding: 0, 
-
-  }, 
+    padding: 0,
+  },
   switch: {
-    marginBottom: 0
+    marginBottom: 0,
   },
-  cardAdditionalSettings:{
+  cardAdditionalSettings: {
     height: 300,
-    width: "100%"
+    width: "100%",
   },
-  dropdownContainer:{
-
-    alignItems:"center",
+  dropdownContainer: {
+    alignItems: "center",
     borderRadius: 25,
     borderWidth: 1,
-
   },
-  dropdown:{
+  dropdown: {
     width: 130,
     height: 40,
-    alignContent: "center", 
+    alignContent: "center",
     borderWidth: 1,
     paddingLeft: 20,
-    fontSize: 10
+    fontSize: 10,
   },
-  cardText2:{
+  cardText2: {
     fontSize: 20,
-    marginTop: 3
-  }, 
-  additionalText:{
+    marginTop: 3,
+  },
+  additionalText: {
     textAlign: "left",
     marginBottom: 10,
-    textDecorationLine: 'underline'
+    textDecorationLine: "underline",
   },
-  additionalInfoContainer:{
-    width: "100%", 
-    marginBottom: 20
-  }, 
+  additionalInfoContainer: {
+    width: "100%",
+    marginBottom: 20,
+  },
 
-  buttonCustom:{
+  buttonCustom: {
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
     height: Dimensions.get("window").height > 600 ? 100 : 50,
-  }
-
+  },
 });
 export default SettingsScreen;
